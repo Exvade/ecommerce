650e4c4441565c6467fa1cbdf080e72f6aceec0f
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-12-11 22:02:17 +0700

    ui: 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom"
+import ErrorPNG from "../assets/error.png"
+
 export default function NotFound() {
   return (
-    <h1>404 not found</h1>
+    <div className="h-[70vh] flex justify-center items-center px-4 flex-col mt-12">
+      <img src={ErrorPNG} alt="404 Picture" className="max-w-lg" />
+      <div className="mt-8 text-center">
+        <h1 className="text-lg font-bold md:text-3xl">Oops, wrong turn! But don't worry</h1>
+        <p className="mb-8 text-sm md:text-base">It seems this page doesn’t fit, but maybe our other collections will be the perfect match for you.</p>
+        <Link to="/" className="self-center flex-shrink px-12 py-3 text-base font-semibold text-white rounded-md bg-primary">Back to Home</Link>
+      </div>
+    </div>
   )
 }
